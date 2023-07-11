edf83d2cb9cf59bc50235c1f6c292ca721b5346b
Bhushan Dhiware <dhiwarebhushan9@gmail.com>
2023-07-11 09:39:01 +0530

    Validator update

--- controllers/validators.js
+++ controllers/validators.js
@@ -5,7 +5,7 @@ const validateSignUpReq = [body('first_name')
     .withMessage("first_Name is required"),
     body('email')
     .notEmpty()
-    .withMessage('Please Enter Valid Mobile Number'),
+    .withMessage('Please enter valid email'),
     body('password')
     .isLength({ min: 6, })
     .withMessage('Please Maintain minimum length')
